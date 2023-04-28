de9c4d9865e0e1879a7b8765dfb819c7f42e34b4
Eric Zorn <zornwebdev@gmail.com>
2023-04-28 17:34:58 -0400

    feat: adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { type ReactNode } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
 import '../styles/global.scss';
 
 interface AppLayoutProps {
@@ -11,21 +13,27 @@ const AppLayout = ({ children }: AppLayoutProps) => {
   const titleText = `Eric Zorn - Senior Software Engineer | Resume ${year}`;
 
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{titleText}</title>
-      </head>
-      <body>
-        <main className="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base">
-          <div className="bg-light-background dark:bg-dark-background w-full h-screen p-2">
-            {children}
-          </div>
-        </main>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>{titleText}</title>
+        </head>
+        <body>
+          <main className="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base">
+            <div className="bg-light-background dark:bg-dark-background w-full h-screen p-2">
+              {children}
+            </div>
+          </main>
+        </body>
+      </html>
+      <Analytics />
+    </>
   );
 };
 
